f9202df0903b185e32e3dc4de418fe95b4eb2b58
eavgrey <81044186+eavgrey@users.noreply.github.com>
2022-10-09 15:19:27 +0300

    Minus, divide, multiply

--- js/ibdex.js
+++ js/ibdex.js
@@ -3,13 +3,13 @@ let numbers = document.querySelectorAll('.num')
 let display = document.getElementById('display')
 let theNum = ""
 let clear = document.getElementById('clear')
-let multiple = document.getElementById('multiply')
 
 
 let clearFunc = () => { 
    theNum = ""
     display.innerHTML = "0"
 }   
+
 
 
 numbers.forEach(button=> {
@@ -30,14 +30,38 @@ let operator = ""
 let currentNum = display.innerHTML
 
  function plus (){
-    currentNum = display.innerHTML//6
+    currentNum = display.innerHTML
     clearFunc()
 operator = "plus"
  }
 
+function minus(){
+    currentNum = display.innerHTML
+    clearFunc()
+operator = "minus"
+}
+
+function divide(){
+    currentNum = display.innerHTML
+    clearFunc()
+operator = "divide"
+}
+
+function multiple(){
+    currentNum = display.innerHTML
+    clearFunc()
+operator = "multiple"
+}
 
  function equal (){
 if( operator === "plus") {
    display.innerHTML = parseFloat(currentNum)  + parseFloat(display.innerHTML) //63
-} 
+} else if( operator === "minus"){
+    display.innerHTML = parseFloat(currentNum)  - parseFloat(display.innerHTML)
+} else if( operator === "divide"){
+    display.innerHTML = parseFloat(currentNum)  / parseFloat(display.innerHTML)
+} else if( operator === "multiple"){
+    display.innerHTML = parseFloat(currentNum)  * parseFloat(display.innerHTML)
+}
  }
+
